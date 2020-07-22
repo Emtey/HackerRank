b20dbcf26c4385707b1510d864e413c9f3aa1e78
Emtey <54373536+Emtey@users.noreply.github.com>
2020-07-22 11:30:27 -0500

    Add files via upload

---
+++ ArrayManipulation.cs
@@ -0,0 +1,70 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries)
+    {
+        long myValue = 0;
+        long count = 0;
+        long[] myArray = new long[n + 1];
+        long rows = queries.GetLength(0);
+
+        for (long i = 0; i < rows; i++)
+        {
+            long a = queries[i][0] - 1; //make it zero based index
+            long b = queries[i][1];
+            long k = queries[i][2];
+            myArray[a] += k;
+            myArray[b] -= k;
+        }
+
+        foreach (long x in myArray)
+        {
+            count += x;
+            if (count > myValue)
+                myValue = count;
+        }
+
+        return myValue;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nm = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nm[0]);
+
+        int m = Convert.ToInt32(nm[1]);
+
+        int[][] queries = new int[m][];
+
+        for (int i = 0; i < m; i++)
+        {
+            queries[i] = Array.ConvertAll(Console.ReadLine().Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
