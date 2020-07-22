b26a91709b1a8bc443dcc5bf7d8914cd83c76f5f
Emtey <54373536+Emtey@users.noreply.github.com>
2020-07-22 11:05:52 -0500

    Add files via upload

---
+++ MinimumSwaps2.cs
@@ -0,0 +1,59 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the minimumSwaps function below.
+    //Counts the minimun number of swaps need to put an array in ascending order.
+    static int minimumSwaps(int[] arr)
+    {
+        int i = 0;
+        int count = 0;
+        int temp = 0;
+
+        while (i < arr.Length)
+        {
+            if (arr[i] != i + 1)
+            {
+                temp = arr[i];
+                arr[i] = arr[temp - 1];
+                arr[temp - 1] = temp;
+                count++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return count;
+    }
+
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int res = minimumSwaps(arr);
+
+        textWriter.WriteLine(res);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
